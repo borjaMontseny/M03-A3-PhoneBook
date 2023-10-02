155cf0e5ac061d64d1a9b496fa6cdcf035bf56b4
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2023-10-02 17:34:58 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -15,18 +15,15 @@ public class Main {
 			System.out.print("Name: ");
 			targetName = sc.nextLine();
 			entry = pb.search(targetName);
-			
+
 			if (entry != null) {
 				System.out.println("The number is " + entry.getPhone() + "\n");
 			} else if (targetName.equalsIgnoreCase("quit")) {
+				System.out.println("Good Bye.");
 			} else {
 				System.out.println("Name not found\n");
 			}
 		}
-
-		System.out.println("Good Bye.");
-
-
 
 		sc.close();
 	}
